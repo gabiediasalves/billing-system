69dd4d1506705cd77ded0baaa84da57ecafd374a
gabiediasalves <gabiediasalves@gmail.com>
2023-02-14 22:52:58 -0300

    add backend

--- src/components/pages/NewWallet.jsx
+++ src/components/pages/NewWallet.jsx
@@ -2,10 +2,30 @@ import { useNavigate } from "react-router-dom"
 import WalletForm from "../wallet/WalletForm"
 
 function NewWallet(){
+
+const navigate = useNavigate();
+
+function createPost(project){
+  
+  fetch('http://localhost:3001/wallets', {
+    method: "POST",
+    headers: {
+      'Content-type': 'application/json'
+    },
+    body: JSON.stringify(project)
+  })
+  .then((resp) => resp.json())
+  .then((data) => {
+    console.log(data)
+    navigate('/wallets', { state: { message: 'Projeto criado com sucesso!' } })
+  })
+  .catch(err => console.log(err))
+
+}
   return (
   <div>
     <p>Criar carteira</p>
-    <WalletForm /*handleSubmit={createPost}*/ btnText="Criar projetos" />
+    <WalletForm handleSubmit={createPost} btnText="Criar projetos" />
   </div>
     )
 }
--- src/components/wallet/WalletForm.jsx
+++ src/components/wallet/WalletForm.jsx
@@ -1,17 +1,50 @@
+import { useState } from "react"
 import Input from "../form/Input"
 import Submit from "../form/Submit"
 
-function WalletForm(){
+
+function WalletForm({handleSubmit, btnText, projectData}) {
+
+const [project, setProject] = useState(projectData || {})
+
+const submit = (e) => {
+  e.preventDefault()
+   console.log(project)
+  handleSubmit(project)
+}
+
+function handleChange(e) {
+  setProject({ ...project, [e.target.name]: e.target.value })
+}
+
   return (
-    <form /*onSubmit={submit}*/>
+    <form onSubmit={submit}>
       <Input
-      type="text"
-      text="Nome da carteira"
-      name="name"
-      placeholder="Insira o nome da carteira"
-     /* handleOnChange={handleChange}*/
-      // value={project.name ? project.name : ''}
+        type="text"
+        text="Nome da carteira"
+        name="name"
+        placeholder="Insira o nome da carteira"
+       handleOnChange={handleChange}
+       value={project.name ? project.name : ''}
       />
+
+      <Input
+        type="number"
+        text="CNPJ"
+        name="CNPJ"
+        placeholder="Insira o CNPJ"
+       handleOnChange={handleChange}
+      />
+
+      <Input
+        type="text"
+        text="Endereço"
+        name="endereco"
+        placeholder="Insira o endereço"
+       handleOnChange={handleChange}
+      />
+
+      <Submit text={btnText} />
 
     </form>
   )
